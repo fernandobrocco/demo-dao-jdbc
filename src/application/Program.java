a935fe42c6077037039c567bdb83b4bf77bd04e0
Fernando Brocco <fernandobrcc@hotmail.com>
2025-03-20 14:26:57 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,12 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TEST 3: seller findById ===");
+		list = sellerDao.findByDepartment(department);
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
